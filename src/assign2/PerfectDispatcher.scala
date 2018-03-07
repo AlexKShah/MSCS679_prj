8697fe75e6cb040138b2e2bd0a30a56d6650e7bd
Alexander Shah <AlexKShah@gmail.com>
2018-03-07 16:12:43 -0500

    not tested, worker generally done, need to properly pass and unpack partitions and result case class types in tasks and get some output

--- src/assign2/PerfectDispatcher.scala
+++ src/assign2/PerfectDispatcher.scala
@@ -1,77 +1,89 @@
+//Alex Shah
+//MSCS 679 - Assignment 2
+
 package assign2
 
 import org.apache.log4j.Logger
 import parascale.actor.last.{Dispatcher, Task}
 import parascale.util._
 
-/**
-  * Spawns a dispatcher to connect to multiple workers.
-  */
-
 case class Partition(start: Long, end: Long, candidate: Long)
   extends Serializable
 
 case class Result(sum: Long, t0: Long, t1: Long) extends Serializable
 
+/**
+  * Spawns a dispatcher to connect to multiple workers.
+  */
+
 object PerfectDispatcher extends App {
   val LOG = Logger.getLogger(getClass)
   LOG.info("started")
-
   // For initial testing on a single host, use this socket.
   // When deploying on multiple hosts, use the VM argument,
   // -Dsocket=<ip address>:9000 which points to the second
   // host.
-  val socket2 = getPropertyOrElse("socket","localhost:9000")
-
+  val socket2 = getPropertyOrElse("socket", "localhost:9000")
   // Construction forks a thread which automatically runs the actor act method.
   new PerfectDispatcher(List("localhost:8000", socket2))
 }
 
 /**
   * Template dispatcher which tests readiness of
+  *
   * @param sockets
   */
 class PerfectDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
+
   import PerfectDispatcher._
+
+  val candidates: List[Long] =
+    List(
+      6,
+      28,
+      496,
+      8128,
+      33550336,
+      33550336 + 1,
+      8589869056L + 1,
+      8589869056L,
+      137438691328L,
+      2305843008139952128L)
 
   /**
     * Handles actor startup after construction.
     */
   def act: Unit = {
-    LOG.info("sockets to workers = " + sockets)
-
-    (0 until sockets.length).foreach { k =>
-      LOG.info("sending message to worker " + k)
-      workers(k) ! "to worker(" + k + ") hello from dispatcher"
-    }
-
-    // TODO: Replace the code below to implement PNF
-    // Create the partition info and put it in two separate messages,
-    // one for each worker, then wait below for two replies, one from
-    // eah worker
     (0 until candidates.length).foreach { index =>
       val candidate = candidates(index)
-      val aTask = Partition(0, candidate/2-1, candidate) //to host A
-      val bTask = Partition(candidate/2, candidate, candidate) //to host B
-      // a ! aTask
-      // b ! bTask, see above 0 until sockets
 
-      while (true)
-      // This while loop wait forever but we really just need to wait
-      // for two replies, one from each worker. The result, that is,
-      // the partial sum and the elapsed times are in the payload as
-      // a Result class.
-        val sum = 0
-        receive match {
-          case task: Task if task.kind == Task.REPLY =>
-            LOG.info("received reply " + task)
-            task.payload match {
-              case result: Result =>
-                sum+result=sum
-            }
-        }
+      val aTask = Partition(0, candidate / 2 - 1, candidate)
+      val bTask = Partition(candidate / 2, candidate, candidate)
+
+      LOG.info("sockets to workers = " + sockets)
+
+      (0 until sockets.length).foreach { k =>
+        LOG.info("sending message to worker " + k)
+        workers(0) ! aTask
+        workers(1) ! bTask
+      }
+    }
+
+    while (true) {
+      var sum = 0
+      receive match {
+        case task: Task if task.kind == Task.REPLY =>
+          LOG.info("received reply " + task)
+          task.payload match {
+            case result: Result =>
+              val (partsum, t0, t1) = result
+              sum = sum + partsum
+          }
+
+          val answer = if (sum == (candidate * 2)) "YES" else "NO"
+          println("Is " + candidate + " perfect? " + answer
+      }
     }
   }
-
 }
 
--- src/assign2/PerfectWorker.scala
+++ src/assign2/PerfectWorker.scala
@@ -1,9 +1,16 @@
+//Alex Shah
+//MSCS 679 - Assignment 2
+
 package assign2
 
 import org.apache.log4j.Logger
 import parascale.actor.last.{Task, Worker}
 import parascale.util._
 
+case class Partition(start: Long, end: Long, candidate: Long)
+  extends Serializable
+
+case class Result(sum: Long, t0: Long, t1: Long) extends Serializable
 
 /**
   * Spawns workers on the localhost.
@@ -51,41 +58,36 @@ class PerfectWorker(port: Int) extends Worker(port) {
 
     // Wait for inbound messages as tasks
     while (true) {
-      receive match {}
+      receive match {
+        case task: Task =>
+          LOG.info("got task = " + task + " sending reply")
+          //get Partition out of task
+          val part = task.payload
+          val partialResult = getPartialResult(part)
+          sender ! partialResult
+      }
+    }
 
-      //need to receive instructions as Partition, then process
-
-      // TODO: Replace the code below to implement PNF
-      // It gets the partition range info from the task payload then
-      // spawns futures (or uses parallel collections) to analyze the
-      // partition in parallel. Finally, when done, it replies
-      // with the partial sum and the time elapsed time.
-
+    def getPartialResult(part: Partition): Result = {
+      val (start, end, candidate) = part
       val RANGE = 1000000L
-
-      val numPartitions = (candidate.toDouble / RANGE).ceil.toInt
-
+      val numPartitions = (end.toDouble / RANGE).ceil.toInt
       val futures = for (k <- 0L until numPartitions) yield Future {
         val lower: Long = k * RANGE + 1
-
-        val upper: Long = candidate min (k + 1) * RANGE
-
+        val upper: Long = end min (k + 1) * RANGE
         sumOfFactorsInRange_(lower, upper, candidate)
       }
 
       val total = futures.foldLeft(0L) {
         (sum, future) =>
+          val t0 = System.nanoTime()
           import scala.concurrent.duration._
-          val result = Await.result(future, 100 seconds)
-
-          sum + result
+          val futureresult = Await.result(future, 100 seconds)
+          sum = sum + futureresult
+          val t1 = System.nanoTime()
+          val partialresult = Result(sum, t0, t1)
+          partialresult
       }
-
-      case task: Task =>
-        LOG.info("got task = " + task + " sending reply")
-
-        // Send a simple reply to test the connectivity.
-        sender ! name + " READY (id=" + id + ")"
     }
 
     def sumOfFactorsInRange_(lower: Long, upper: Long, number: Long): Long = {
