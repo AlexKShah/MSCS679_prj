f5cdb81f80828044b836923fe99ce09016f7be51
Alexander Shah <AlexKShah@gmail.com>
2018-04-26 12:38:50 -0400

    template out assn3

--- assign3/src/ParaDispatcher.scala
+++ assign3/src/ParaDispatcher.scala
@@ -1,16 +1,46 @@
-import org.apache.log4j.Logger
-import parascale.actor.last.{Dispatcher, Task}
-import parascale.util._
-import parabond.cluster.Partition
+import parascale.actor.last.Dispatcher
+import parascale.util.getPropertyOrElse
+import parabond.cluster._
 
 case class Result(t0: Int, t1: Int) extends Serializable
+//not in parabond.cluster?
+case class Partition(n: Long, begin: Long) extends Serializable
 
 object ParaDispatcher extends App {
+
   class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
-    import ParaDispatcher._
 
     def act: Unit = {
 
+      /*
+      a. Output the report header.
+      b. Get the next n, that is, number of portfolios to price.
+      c. Reset the check portfolio prices.
+      d. Create two workers by passing the dispatcher constructor two sockets.
+      e. Create two partitions:
+        A) Partition(seed=0, n=n/2, begin=0) and
+        B) Partition(seed=0, n=n/2, begin=n/2)
+      f. Send worker(0) the first partition and worker(1) the second partition.
+      g. Wait for results.
+      h. Test the check portfolios.
+      i. Output the performance statistics.
+      j. Repeat step b = Get the next n, that is, number of portfolios to price.
+       */
+
+      //a
+      println("header TODO")
+
+      //b
+      val nportf = getPropertyOrElse("nportf", "1000")
+
+      val ramp = List(1000, 2000, 4000, 8000, 16000, 32000, 64000, 100000)
+
+      ramp.foreach { n =>
+        workers(0) ! Partition(n/2, 0)
+        workers(1) ! Partition(n/2, n/2)
+      }
+
     }
   }
+
 }
--- assign3/src/ParaWorker.scala
+++ assign3/src/ParaWorker.scala
@@ -1,8 +1,7 @@
-import parabond.cluster
 import org.apache.log4j.Logger
-import parascale.actor.last.{Task, Worker}
+import parascale.actor.last.Worker
 import parascale.util._
-import parabond.cluster.Partition
+import parabond.cluster._
 
 object ParaWorker extends App {
 
@@ -12,5 +11,15 @@ class ParaWorker(port: Int) extends Worker(port) {
   import ParaWorker._
   override def act: Unit = {
 
+    /*
+    a. If worker running on a single host, spawn two workers else spawn one worker.
+    b. Wait for a task.
+    c. Create a Partition.
+    d. Create a Node with the Partition.
+    e. Invoke analyze on the Node and wait for it to finish.
+    f. Reduce the partial T1 results.
+    g. Reply to dispatcher with T1
+    h. Repeat step b = Wait for a task.
+     */
   }
 }
