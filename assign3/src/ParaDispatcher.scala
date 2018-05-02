659dd3637cd06ef74b1fc89ec53cc0c1843217e9
Alexander Shah <AlexKShah@gmail.com>
2018-05-02 10:17:53 -0400

    update comments

--- assign3/src/ParaDispatcher.scala
+++ assign3/src/ParaDispatcher.scala
@@ -1,3 +1,13 @@
+/**
+  * Assignment 3
+  * Alex Shah
+  * MSCS 679
+  * 5/1/18
+  *
+  * ParaDispatcher sends partitions to workers to calculate portfolio cost
+  * Receives work from workers and reports time and statistics
+  */
+
 import org.apache.log4j.{Level, Logger}
 import parascale.actor.last.{Dispatcher, Task}
 import parascale.util.getPropertyOrElse
@@ -5,15 +15,24 @@ import parabond.cluster.{Analysis, Partition, check, checkReset}
 import parabond.cluster._
 
 object ParaDispatcher extends App {
+  //initiate logger
   val LOG = Logger.getLogger(getClass)
   Logger.getRootLogger.setLevel(Level.OFF)
-  //-Dnhosts=2 -Dsocket=IP:socket
   val socket2 = getPropertyOrElse("socket", "localhost:9000")
+  //create new dispatcher
   new ParaDispatcher(List("localhost:8000", socket2))
 }
 
 class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
 
+  /**
+    * print report header, for next n portfolios:
+    *   reset ids,
+    *   start timing tn,
+    *   send work to workers,
+    *   sum up t1 time from replies,
+    *   print statistics for that rung
+    */
   def act: Unit = {
     import ParaDispatcher._
 
@@ -24,11 +43,12 @@ class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
     val numCores = Runtime.getRuntime().availableProcessors() //8
     println("Cores: " + numCores)
     println("Number of workers: " + workers.length)
+    //TODO hosts
     println;
     println("Memory Bound Node Report:");
     println;
 
-    //Header
+    //Table header
     println(String.format("%-9s", "n") +
       String.format("%-9s", "Missed") +
       String.format("%-9s", "T1") +
@@ -40,6 +60,7 @@ class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
     //b. Get the next n, that is, number of portfolios to price.
     val n = getPropertyOrElse("n", 100)
 
+    //amount of portfolios to test
     val ladder = List(1000, 2000, 4000, 8000, 16000, 32000, 64000, 100000)
 
     //j. Repeat step b for each rung
@@ -64,7 +85,7 @@ class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
       //receive replies from the 2 workers
       val replies = for (k <- 0 until workers.length) yield receive
 
-      // sum up partial T1's
+      // sum up partial T1's from worker replies
       val T1 = replies.foldLeft(0L) { (sum, reply) =>
         reply match {
           case task: Task if (task.kind == Task.REPLY) =>
@@ -75,8 +96,9 @@ class ParaDispatcher(sockets: List[String]) extends Dispatcher(sockets) {
         }
       } seconds
 
-      //i. Output the performance statistics.
       val t1 = System.nanoTime()
+
+      //i. Output the performance statistics.
       val TN = (t1 - t0) seconds
       val speedup = T1 / TN
       val e = speedup / numCores
--- assign3/src/ParaWorker.scala
+++ assign3/src/ParaWorker.scala
@@ -1,11 +1,21 @@
+/**
+  * Assignment 3
+  * Alex Shah
+  * MSCS 679
+  * 5/1/18
+  *
+  * ParaWorker receives partitions from dispatcher to calculate portfolio cost
+  * sends back the time it took
+  */
+
 import org.apache.log4j.Logger
 import parascale.actor.last.{Task, Worker}
 import parascale.util._
 import parabond.cluster._
 import parascale.parabond.util.Result
-//import parascale.parabond.util
 
 object ParaWorker extends App {
+  //initiate Logger
   val LOG = Logger.getLogger(getClass)
   LOG.info("started")
 
@@ -13,7 +23,7 @@ object ParaWorker extends App {
   val nhosts = getPropertyOrElse("nhosts", 1)
 
   //receive parameter to change node type, defaults to basic node
-  val prop = getPropertyOrElse("node","parabond.cluster.MemoryBoundNode")
+  val prop = getPropertyOrElse("node","parabond.cluster.BasicNode")
   val clazz = Class.forName(prop)
   import parabond.cluster.Node
   val node = clazz.newInstance.asInstanceOf[Node]
@@ -31,13 +41,23 @@ object ParaWorker extends App {
   // When deploying on two hosts, "ports" will contain one port per host.
   for (port <- ports) {
     // Construction forks a thread which automatically runs the actor act method.
+    //start up new worker
     new ParaWorker(port)
   }
 }
 
 class ParaWorker(port: Int) extends Worker(port) {
   import ParaWorker._
+
+  /**
+    * continuously wait for work to:
+    *   get a partition out of the task
+    *   analyze the partition with the chosen node type
+    *   sum up the time it took for the work
+    *   send back the partial t1 time to dispatcher
+    */
   override def act: Unit = {
+
     //b. Wait for a task.
     while (true) {
       receive match {
@@ -53,23 +73,13 @@ class ParaWorker(port: Int) extends Worker(port) {
           //e. Invoke analyze on the Node and wait for it to finish.
           val analysis = node analyze(part)
 
-          //report(LOG, analysis, checkIds)
-
+          //sum up workers time to create partial t1
           val partialT1 = analysis.results.foldLeft(0L) { (sum, job)=>
             sum + (job.result.t1 - job.result.t0)
           }
+          //send back the partial time for t1
           sender ! Result(partialT1)
       }
     }
-    /*
-    a. If worker running on a single host, spawn two workers else spawn one worker.
-    b. Wait for a task.
-    c. Create a Partition.
-    d. Create a Node with the Partition.
-    e. Invoke analyze on the Node and wait for it to finish.
-    f. Reduce the partial T1 results.
-    g. Reply to dispatcher with T1
-    h. Repeat step b = Wait for a task.
-     */
   }
 }
